6b3a9a6d8ce3de7255ef7bea192e0630e8e5635f
jfranmatheu <45881831+jfranmatheu@users.noreply.github.com>
2022-09-03 01:30:06 +0200

    update. second implementation

--- jfracture/__init__.py
+++ jfracture/__init__.py
@@ -16,7 +16,7 @@ bl_info = {
     "author" : "JF racture",
     "description" : "Fast Cell Fracture",
     "blender" : (3, 2, 0),
-    "version" : (0, 1, 0),
+    "version" : (0, 2, 0),
     "location" : "",
     "warning" : "",
     "category" : "Generic"
